8a330972e25945b05808c45f7a209964101100a8
Jonathan Løseth <jonathhl@stud.ntnu.no>
2022-03-28 15:56:04 +0200

    (GUI) Started working on expandable boxes for main side bar

---
+++ frontend/src/components/SideBarComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col col="12">
+        <div>
+          <div class="d-flex">
+            <v-expansion-panels v-model="panel" :disabled="disabled" multiple>
+              <v-expansion-panel>
+                <v-expansion-panel-header>Subjects</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <!-- TODO: Add buttons -->
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+            </v-expansion-panels>
+          </div>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {};
+</script>
