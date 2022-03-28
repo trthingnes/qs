de761727bdd52ab2972e7829dc61f48a58889eaf
Jonathan Løseth <jonathhl@stud.ntnu.no>
2022-03-28 19:41:28 +0200

    Mostly finished with the HTML part

--- frontend/src/components/SideBarComponent.vue
+++ frontend/src/components/SideBarComponent.vue
@@ -8,9 +8,42 @@
               <v-expansion-panel>
                 <v-expansion-panel-header>Subjects</v-expansion-panel-header>
                 <v-expansion-panel-content>
-                  <!-- TODO: Add buttons -->
+                  <v-btn
+                    v-for="({ subjectName }, i) in { subjectList }"
+                    :key="i"
+                  >
+                    { subjectName }
+                  </v-btn>
                 </v-expansion-panel-content>
               </v-expansion-panel>
+
+              <v-expansion-panel>
+                <v-expansion-panel-header>
+                  Queue administration
+                </v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-btn
+                    v-for="({ subjectName }, i) in { subjectList }"
+                    :key="i"
+                  >
+                    { subjectName }
+                  </v-btn>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+
+              <v-expansion-panel>
+                <v-expansion-panel-header>Task list</v-expansion-panel-header>
+                <v-expansion-panel-content>
+                  <v-btn
+                    v-for="({ subjectName }, i) in { subjectList }"
+                    :key="i"
+                  >
+                    { subjectName }
+                  </v-btn>
+                </v-expansion-panel-content>
+              </v-expansion-panel>
+              <v-btn>User settings</v-btn>
+              <v-btn>Log out</v-btn>
             </v-expansion-panels>
           </div>
         </div>
@@ -20,5 +53,16 @@
 </template>
 
 <script>
-export default {};
+export default {
+  name: "SideBar",
+  data() {
+    return {
+      subjectName: null,
+      subjectList: [],
+    };
+  },
+  methods: {
+    loadSubjects() {},
+  },
+};
 </script>
