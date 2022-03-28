51c99783ecc1d048f4f2e1a8ed6c1512a7752680
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2022-03-28 13:39:14 +0200

    Ran Spring Initializr

---
+++ backend/src/main/kotlin/edu/ntnu/idatt2105/g16/backend/BackendApplication.kt
@@ -0,0 +1,11 @@
+package edu.ntnu.idatt2105.g16.backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendApplication>(*args)
+}
