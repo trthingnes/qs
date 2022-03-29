1ac0641f30b59dfd337b5cf607f114beefdc561d
Jonathan Løseth <jonathhl@stud.ntnu.no>
2022-03-29 14:27:04 +0200

    Created simple list design for the actual queue.

---
+++ frontend/src/views/QueueView.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+        <div>
+            <div></div>
+            <div></div>
+        </div>
+        <div>
+            <v-card class="mx-auto mr-4" max-height="300">
+                <v-list density="compact">
+                    <v-list-subheader>QUEUE</v-list-subheader>
+                    <v-list-item
+                        v-for="(student, i) in queue"
+                        :key="i"
+                        :value="student"
+                    >
+                        <v-list-item-title
+                            v-text="student.name"
+                        ></v-list-item-title>
+                    </v-list-item>
+                </v-list>
+            </v-card>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "QueueView",
+    data: () => ({
+        queue: [{ name: "Jonathan" }, { name: "Tobias" }, { name: "Tor" }],
+    }),
+}
+</script>
+
+<style scoped></style>
