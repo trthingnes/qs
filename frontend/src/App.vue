077105e49e861776775ca1261ef3db390cbaaff9
Tobias Rødahl Thingnes <tobiasth@stud.ntnu.no>
2022-03-29 22:20:24 +0200

    Tried to make a component for navigation

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,28 +1,24 @@
 <template>
+    <!-- This is the recommended layout according to the docs. -->
     <v-app>
+        <NavigationDrawerComponent />
+
         <v-main>
-            <v-row>
-                <!-- A row has 12 columns, so to fill the whole screen the columns should sum to 12. -->
-                <v-col cols="2" class="pr-0">
-                    <SideBar />
-                    <!-- Show sidebar constantly -->
-                </v-col>
-                <v-col cols="10" class="pl-0">
-                    <router-view />
-                    <!-- Show whatever router view is currently active. -->
-                </v-col>
-            </v-row>
+            <v-container fluid>
+                <!-- Show whatever router view is currently active. -->
+                <router-view />
+            </v-container>
         </v-main>
     </v-app>
 </template>
 
 <script>
-import SideBar from "@/components/SideBarComponent.vue"
+import NavigationDrawerComponent from "@/components/NavigationDrawerComponent.vue"
 
 export default {
     name: "App",
     components: {
-        SideBar,
+        NavigationDrawerComponent,
     },
 }
 </script>
---
+++ frontend/src/components/NavigationDrawerComponent.vue
@@ -0,0 +1,88 @@
+<template>
+    <v-navigation-drawer width="350" app>
+        <v-list>
+            <v-list-item>
+                <h1>Qs</h1>
+            </v-list-item>
+
+            <v-divider class="mt-2 mb-2"></v-divider>
+
+            <v-list-subheader color="black">
+                <v-icon icon="mdi-school" class="mr-3"></v-icon>
+                Dine fag
+            </v-list-subheader>
+            <v-list-item
+                v-for="[code, name] in studentCourses"
+                :title="name"
+                :value="code"
+                :key="code"
+                @click.stop="onCourseClick(code)"
+            ></v-list-item>
+
+            <div v-if="assistantCourses.length">
+                <v-divider class="mt-2 mb-2"></v-divider>
+
+                <v-list-subheader color="black">
+                    <v-icon icon="mdi-school" class="mr-3"></v-icon>
+                    Læringsassistent
+                </v-list-subheader>
+                <v-list-item
+                    v-for="[code, name] in assistantCourses"
+                    :title="name"
+                    :value="code"
+                    :key="code"
+                    @click.stop="onCourseClick(code)"
+                ></v-list-item>
+            </div>
+        </v-list>
+
+        <template v-slot:append>
+            <v-divider></v-divider>
+            <v-card class="mt-3 pb-2" density="compact">
+                <v-card-title> Ola Nordmann </v-card-title>
+                <v-card-subtitle> olanord@stud.ntnu.no </v-card-subtitle>
+                <v-card-actions>
+                    <v-btn
+                        variant="contained-text"
+                        append-icon="mdi-cog"
+                        class="ml-2"
+                    >
+                        Innstillinger
+                    </v-btn>
+                    <v-btn
+                        variant="contained-text"
+                        append-icon="mdi-logout"
+                        class="ml-2"
+                    >
+                        Logg ut
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </template>
+    </v-navigation-drawer>
+</template>
+<script>
+import { useRouter } from "vue-router"
+
+export default {
+    setup() {
+        const router = useRouter()
+
+        const studentCourses = [
+            ["IDATT2101", "Algoritmer og datastrukturer"],
+            ["IDATT2105", "Full-stack applikasjonsutvikling"],
+        ]
+
+        const assistantCourses = [
+            ["IDATT1001", "Programmering 1"],
+            ["IDATT2001", "Programmering 2"],
+        ]
+
+        const onCourseClick = (code) => {
+            router.push(`/courses/${code}`)
+        }
+
+        return { studentCourses, assistantCourses, onCourseClick }
+    },
+}
+</script>
