3d8080ae2c9027269b1daff34593008554363be0
Jonathan Løseth <jonathhl@stud.ntnu.no>
2022-03-29 12:21:22 +0200

    Stylized some more.

--- frontend/src/components/SideBarComponent.vue
+++ frontend/src/components/SideBarComponent.vue
@@ -1,99 +1,117 @@
 <template>
-  <v-expansion-panels>
-    <v-expansion-panel>
-      <v-expansion-panel-title>
-        <v-row no-gutters @click="loadSubjects">
-          <v-col cols="4" class="d-flex justify-start"> Subjects </v-col>
-        </v-row>
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <v-item-group selected-class="bg-primary">
-          <v-container>
-            <v-row>
-              <v-col
-                v-for="subject in subjectList"
-                :key="subject"
-                cols="12"
-                md="4"
-              >
-                <v-item>
-                  <v-card :class="['d-flex']" dark>
-                    <div class="text-h6 flex-grow-1">
-                      {{ subject }}
-                    </div>
-                  </v-card>
-                </v-item>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-item-group>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
+  <div>
+    <div class="ml-3 mr-3 bg-light-blue">
+      <div class="ml-5 mt-3 text-white">
+        <h1>Qs</h1>
+      </div>
+      <div class="ml-5 pb-1 text-white">
+        <p>"Username"</p>
+      </div>
+    </div>
 
-    <v-expansion-panel>
-      <v-expansion-panel-title>
-        <v-row no-gutters @click="loadSubjects">
-          <v-col cols="4" class="d-flex justify-start">
-            Queue Administration
-          </v-col>
-        </v-row>
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <v-item-group selected-class="bg-primary">
-          <v-container>
-            <v-row>
-              <v-col
-                v-for="subject in subjectList"
-                :key="subject"
-                cols="12"
-                md="4"
-              >
-                <v-item>
-                  <v-card :class="['d-flex']" dark>
-                    <div class="text-h6 flex-grow-1">
-                      {{ subject }}
-                    </div>
-                  </v-card>
-                </v-item>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-item-group>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
+    <v-expansion-panels class="ml-3 mr-3">
+      <v-expansion-panel class="mb-2">
+        <v-expansion-panel-title>
+          <v-row no-gutters @click="loadSubjects">
+            <v-col cols="4" class="d-flex justify-start"> Subjects </v-col>
+          </v-row>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <v-item-group selected-class="bg-primary">
+            <v-container>
+              <v-row>
+                <v-col
+                  v-for="subject in subjectList"
+                  :key="subject"
+                  cols="12"
+                  md="4"
+                >
+                  <v-item>
+                    <v-card :class="['d-flex']" dark>
+                      <div class="text-h6 flex-grow-1">
+                        {{ subject }}
+                      </div>
+                    </v-card>
+                  </v-item>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-item-group>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
 
-    <v-expansion-panel>
-      <v-expansion-panel-title>
-        <v-row no-gutters @click="loadSubjects">
-          <v-col cols="4" class="d-flex justify-start">
-            List of approved tasks
-          </v-col>
-        </v-row>
-      </v-expansion-panel-title>
-      <v-expansion-panel-text>
-        <v-item-group selected-class="bg-primary">
-          <v-container>
-            <v-row>
-              <v-col
-                v-for="subject in subjectList"
-                :key="subject"
-                cols="12"
-                md="4"
-              >
-                <v-item>
-                  <v-card :class="['d-flex']" dark>
-                    <div class="text-h6 flex-grow-1">
-                      {{ subject }}
-                    </div>
-                  </v-card>
-                </v-item>
-              </v-col>
-            </v-row>
-          </v-container>
-        </v-item-group>
-      </v-expansion-panel-text>
-    </v-expansion-panel>
-  </v-expansion-panels>
+      <v-expansion-panel class="mb-2">
+        <v-expansion-panel-title>
+          <v-row no-gutters @click="loadSubjects">
+            <v-col cols="4" class="d-flex justify-start">
+              Queue Administration
+            </v-col>
+          </v-row>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <v-item-group selected-class="bg-primary">
+            <v-container>
+              <v-row>
+                <v-col
+                  v-for="subject in subjectList"
+                  :key="subject"
+                  cols="12"
+                  md="4"
+                >
+                  <v-item>
+                    <v-card :class="['d-flex']" dark>
+                      <div class="text-h6 flex-grow-1">
+                        {{ subject }}
+                      </div>
+                    </v-card>
+                  </v-item>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-item-group>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+
+      <v-expansion-panel class="mb-2">
+        <v-expansion-panel-title>
+          <v-row no-gutters @click="loadSubjects">
+            <v-col cols="4" class="d-flex justify-start">
+              List of approved tasks
+            </v-col>
+          </v-row>
+        </v-expansion-panel-title>
+        <v-expansion-panel-text>
+          <v-item-group selected-class="bg-primary">
+            <v-container>
+              <v-row>
+                <v-col
+                  v-for="subject in subjectList"
+                  :key="subject"
+                  cols="12"
+                  md="4"
+                >
+                  <v-item>
+                    <v-card :class="['d-flex']" dark>
+                      <div class="text-h6 flex-grow-1">
+                        {{ subject }}
+                      </div>
+                    </v-card>
+                  </v-item>
+                </v-col>
+              </v-row>
+            </v-container>
+          </v-item-group>
+        </v-expansion-panel-text>
+      </v-expansion-panel>
+    </v-expansion-panels>
+
+    <div class="mb-2 ml-3">
+      <v-btn>User settings</v-btn>
+    </div>
+    <div class="mt-2 ml-3">
+      <v-btn>Logout</v-btn>
+    </div>
+  </div>
 </template>
 
 <script>
