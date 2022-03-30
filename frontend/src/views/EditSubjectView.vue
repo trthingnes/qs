dffce4ee158aa65bb3e326ee4a9c695392d84bbd
Jonathan Løseth <jonathhl@stud.ntnu.no>
2022-03-30 15:31:55 +0200

    Cleaned up queue code a bit, also started working on view to add and edit subjects.

---
+++ frontend/src/views/EditSubjectView.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-form v-model="valid">
+        <v-container>
+            <v-row>
+                <!-- TODO: Add functionality to all buttons -->
+                <v-col cols="12" md="4">
+                    <v-label>Subject code:</v-label>
+                    <v-text-field
+                        v-model="subjectcode"
+                        :counter="9"
+                        required
+                    ></v-text-field>
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <v-label>Subject name:</v-label>
+                    <v-text-field
+                        v-model="subjectName"
+                        :counter="30"
+                        required
+                    ></v-text-field>
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <!-- TODO: Find way to make this area empty, or find something to put here -->
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <v-label>Number of assignments:</v-label>
+                    <v-slider
+                        v-model="numAssignments"
+                        :min="0"
+                        :max="15"
+                        :step="1"
+                        thumb-label
+                        :disabled="false"
+                    ></v-slider>
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <!-- -->
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <!-- -->
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <v-btn :disabled="false" @click="updateInfo">Save</v-btn>
+                </v-col>
+            </v-row>
+        </v-container>
+    </v-form>
+</template>
+
+<script>
+export default {
+    name: "EditSubjectView",
+    data: () => ({
+        numAssignments: 0,
+    }),
+}
+</script>
+
+<style scoped></style>
--- frontend/src/views/QueueView.vue
+++ frontend/src/views/QueueView.vue
@@ -1,32 +1,27 @@
 <template>
-    <div>
-        <div>
-            <v-card class="mx-auto mr-4">
-                <v-list>
-                    <v-list-subheader
-                        >Students in queue: {{ queue.length }}</v-list-subheader
-                    >
-                    <v-list-item
-                        v-for="(student, i) in queue"
-                        :key="i"
-                        :value="student"
-                        class="text-center"
-                    >
-                        <v-list-item-title
-                            v-text="student.name"
-                        ></v-list-item-title>
-                        <v-spacer></v-spacer>
-                        <v-list-item-content>
-                            {{ student.type }}
-                            <v-spacer></v-spacer>
-                            Assignment:
-                            {{ student.assignment }}</v-list-item-content
-                        >
-                    </v-list-item>
-                </v-list>
-            </v-card>
-        </div>
-    </div>
+    <v-card class="mx-auto mr-4">
+        <v-list>
+            <v-list-subheader
+                >Students in queue: {{ queue.length }}</v-list-subheader
+            >
+            <v-list-item
+                v-for="(student, i) in queue"
+                :key="i"
+                :value="student"
+                class="text-center"
+                @click="regHelp"
+            >
+                <v-list-item-title v-text="student.name"></v-list-item-title>
+                <v-spacer></v-spacer>
+                <v-list-item-content>
+                    {{ student.type }}
+                    <v-spacer></v-spacer>
+                    Assignment:
+                    {{ student.assignment }}</v-list-item-content
+                >
+            </v-list-item>
+        </v-list>
+    </v-card>
 </template>
 
 <script>
@@ -39,6 +34,12 @@ export default {
             { name: "Tor", type: "Help", assignment: "38" },
         ],
     }),
+    methods: {
+        // TODO: Finne mer forklarende metodenavn, finn også rett syntax for å endre fargen.
+        regHelp() {
+            document.documentElement.style.color = "#add8e6"
+        },
+    },
 }
 </script>
 
