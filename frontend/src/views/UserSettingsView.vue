6f0781c63eeddce1dae925645c9aff164d5526a7
Jonathan Løseth <jonathhl@stud.ntnu.no>
2022-03-30 13:28:30 +0200

    Added functionality for updating user information.

--- frontend/src/views/UserSettingsView.vue
+++ frontend/src/views/UserSettingsView.vue
@@ -38,12 +38,31 @@
                         :rules="passwordRules"
                         :type="'password'"
                         label="Password"
+                        disabled="true"
                         required
                     ></v-text-field>
                 </v-col>
 
                 <v-col cols="12" md="4">
-                    <v-btn :disabled="false">Save</v-btn>
+                    <v-text-field
+                        v-model="newPass"
+                        :rules="passwordRules"
+                        :type="'password'"
+                        label="New password"
+                    ></v-text-field>
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <v-text-field
+                        v-model="repPass"
+                        :rules="passwordRules"
+                        :type="'password'"
+                        label="Repeat new password"
+                    ></v-text-field>
+                </v-col>
+
+                <v-col cols="12" md="4">
+                    <v-btn :disabled="false" @click="updateInfo">Save</v-btn>
                 </v-col>
             </v-row>
         </v-container>
@@ -55,7 +74,18 @@ export default {
     name: "UserSettingsView",
     data: () => ({
         valid: true,
-        name: "",
+        firstname: "",
+        lastname: "",
+        email: "",
+        password: "",
+        newPass: "",
+        repPass: "",
+        user: {
+            firstname: "Jonathan",
+            lastname: "Løseth",
+            email: "jonathhl@stud.ntnu.no",
+            password: "admin",
+        },
         nameRules: [
             (v) => !!v || "Name is required",
             (v) =>
@@ -65,8 +95,26 @@ export default {
             (v) => !!v || "Email is required",
             (v) => /.+@.+\..+/.test(v) || "E-mail must be valid",
         ],
+        passwordRules: [
+            (v) => !!v || "Password can not be empty",
+            (v) =>
+                /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/.test(
+                    v
+                ) || "Password must be valid",
+        ],
     }),
+    methods: {
+        updateInfo(firstname, lastname, email, password) {
+            if (this.newPass === this.repPass) password = this.newPass
+
+            this.$store.dispatch("saveUserInfo", {
+                firstname,
+                lastname,
+                email,
+                password,
+            })
+            console.log("Updated user info")
+        },
+    },
 }
 </script>
-
-<style scoped></style>
